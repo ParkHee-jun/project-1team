c89c4d24e1d2087589611637ac84c18c07ac4343
ParkHee-jun <67316098+ParkHee-jun@users.noreply.github.com>
2020-09-16 14:30:09 +0900

    assets
    관리자페이지 - 파일

---
+++ assets/js/index.js
@@ -0,0 +1,106 @@
+$(function() {
+    "use strict";
+
+     // chart 1
+	 
+		  var ctx = document.getElementById('chart1').getContext('2d');
+		
+			var myChart = new Chart(ctx, {
+				type: 'line',
+				data: {
+					labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+					datasets: [{
+						label: 'New Visitor',
+						data: [3, 3, 6, 4, 5, 2, 5, 3, 5, 3],
+						backgroundColor: '#fff',
+						borderColor: "transparent",
+						pointRadius :"0",
+						borderWidth: 3
+					}, {
+						label: 'Old Visitor',
+						data: [8, 7, 13, 9, 12, 8, 10, 7, 11, 8],
+						backgroundColor: "rgba(255, 255, 255, 0.25)",
+						borderColor: "transparent",
+						pointRadius :"0",
+						borderWidth: 1
+					}]
+				},
+			options: {
+				maintainAspectRatio: false,
+				legend: {
+				  display: false,
+				  labels: {
+					fontColor: '#ddd',  
+					boxWidth:40
+				  }
+				},
+				tooltips: {
+				  displayColors:false
+				},	
+			  scales: {
+				  xAxes: [{
+					ticks: {
+						beginAtZero:true,
+						fontColor: '#ddd'
+					},
+					gridLines: {
+					  display: true ,
+					  color: "rgba(221, 221, 221, 0.08)"
+					},
+				  }],
+				   yAxes: [{
+					ticks: {
+						beginAtZero:true,
+						fontColor: '#ddd'
+					},
+					gridLines: {
+					  display: true ,
+					  color: "rgba(221, 221, 221, 0.08)"
+					},
+				  }]
+				 }
+
+			 }
+			});  
+		
+		
+    // chart 2
+
+		var ctx = document.getElementById("chart2").getContext('2d');
+			var myChart = new Chart(ctx, {
+				type: 'doughnut',
+				data: {
+					labels: ["비트코인", "이더리움", "리플", "이오스"],
+					datasets: [{
+						backgroundColor: [
+							"#ffffff",
+							"rgba(255, 255, 255, 0.70)",
+							"rgba(255, 255, 255, 0.50)",
+							"rgba(255, 255, 255, 0.20)"
+						],
+						data: [5856, 2602, 1802, 1105],
+						borderWidth: [0, 0, 0, 0]
+					}]
+				},
+			options: {
+				maintainAspectRatio: false,
+			   legend: {
+				 position :"bottom",	
+				 display: false,
+				    labels: {
+					  fontColor: '#ddd',  
+					  boxWidth:15
+				   }
+				}
+				,
+				tooltips: {
+				  displayColors:false
+				}
+			   }
+			});
+		
+
+		
+		
+   });	 
+   
